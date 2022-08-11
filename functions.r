593f049db0aa9ad7622fcfebfac95171970d313b
darcien-uwb <60338497+darcien-uwb@users.noreply.github.com>
2022-08-10 21:59:42 -0700

    Made changes to Productivity figure. Formatted authorProdOverTime function.

--- functions.r
+++ functions.r
@@ -308,55 +308,68 @@ authorProdOverTime <- function (M, k = 10, graph = TRUE)
 {
   if (!("DI" %in% names(M))) {
     M$DI = "NA"
-  }
+    }
+
   M$TC <- as.numeric(M$TC)
   M$PY <- as.numeric(M$PY)
   M <- M[!is.na(M$PY), ]
   Y <- as.numeric(substr(Sys.time(), 1, 4))
   listAU <- (strsplit(M$AU, ";"))
   nAU <- lengths(listAU)
+
   df <- data.frame(AU = trimws(unlist(listAU)), SR = rep(M$SR,nAU))
   AU <- df %>% group_by(.data$AU) %>% count() %>% arrange(desc(.data$n)) %>% 
     ungroup()
   k <- min(k, nrow(AU))
   AU <- AU %>% slice_head(n = k)
+
   df <- df %>% right_join(AU, by = "AU") %>% left_join(M, by = "SR") %>% 
-    select(.data$AU.x, .data$PY, .data$TI, .data$SO, .data$DI, 
-           .data$TC) %>% mutate(TCpY = .data$TC/(Y - .data$PY + 
-                                                   1)) %>% group_by(.data$AU.x) %>% mutate(n = length(.data$AU.x)) %>% 
-    ungroup() %>% rename(Author = .data$AU.x, year = .data$PY, 
-                         DOI = .data$DI) %>% arrange(desc(.data$n), desc(.data$year)) %>% 
+    select(.data$AU.x, .data$PY, .data$TI, .data$SO, .data$DI, .data$TC) %>% 
+    mutate(TCpY = .data$TC/(Y - .data$PY + 1)) %>% 
+    group_by(.data$AU.x) %>% 
+    mutate(n = length(.data$AU.x)) %>% 
+    ungroup() %>% 
+    rename(Author = .data$AU.x, year = .data$PY, DOI = .data$DI) %>% 
+    arrange(desc(.data$n), desc(.data$year)) %>% 
     select(-.data$n)
+
   df2 <- dplyr::group_by(df, .data$Author, .data$year) %>% 
-    dplyr::summarise(freq = length(.data$year), TC = sum(.data$TC), 
-                     TCpY = sum(.data$TCpY)) %>% as.data.frame()
+    dplyr::summarise(freq = length(.data$year), TC = sum(.data$TC), TCpY = sum(.data$TCpY)) %>% 
+    as.data.frame()
+
   df2$Author <- factor(df2$Author, levels = AU$AU[1:k])
   x <- c(0.5, 1.5 * k/10)
-  y <- c(min(df$year), min(df$year) + diff(range(df2$year)) * 
-           0.125)
+  y <- c(min(df$year), min(df$year) + diff(range(df2$year)) * 0.125)
   #data("logo", envir = environment())
   #logo <- grid::rasterGrob(logo, interpolate = TRUE)
-  g <- ggplot(df2, aes(x = .data$Author, y = .data$year, text = paste("Author: ", 
-                                                                      .data$Author, "\nYear: ", .data$year, "\nN. of Articles: ", 
-                                                                      .data$freq, "\nTotal Citations per Year: ", round(.data$TCpY, 
-                                                                                                                        2)))) + geom_point(aes(alpha = .data$TCpY, size = .data$freq), 
-                                                                                                                                           color = "dodgerblue4") + scale_size(range = c(2, 6)) + 
-    scale_alpha(range = c(0.3, 1)) + scale_y_continuous(breaks = seq(min(df2$year), 
-                                                                     max(df2$year), by = 2)) + guides(size = guide_legend(order = 1, 
-                                                                                                                          "N.Articles"), alpha = guide_legend(order = 2, "TC per Year")) + 
-    theme(legend.position = "right", text = element_text(color = "#444444"), 
+
+  g <- ggplot(df2, aes(x = .data$Author, y = .data$year, 
+                       text = paste("Author: ", .data$Author,
+                                    "\nYear: ", .data$year,
+                                    "\nN. of Articles: ", .data$freq, 
+                                    "\nTotal Citations per Year: ", round(.data$TCpY, 2)))) + 
+    geom_point(aes(alpha = .data$TCpY, size = .data$freq), color = "dodgerblue4") + 
+    scale_size(range = c(2, 6)) + 
+    scale_alpha(range = c(0.3, 1)) + 
+    scale_y_continuous(breaks = seq(min(df2$year), max(df2$year), by = 2)) + 
+    guides(size = guide_legend(order = 1, "Number of\nArticles"), 
+           alpha = guide_legend(order = 2, "Times Cited\nper Year")) + 
+    theme(legend.position = "right", 
+          text = element_text(color = "#444444"), 
           panel.background = element_rect(fill = "#FFFFFF"), 
-          plot.title = element_text(size = 24), axis.title = element_text(size = 14, 
-                                                                          color = "#555555"), axis.title.y = element_text(vjust = 1, 
-                                                                                                                          angle = 90), axis.title.x = element_text(hjust = 0.95), 
+          plot.title = element_text(size = 24), 
+          axis.title = element_text(size = 14, color = "#555555"), 
+          axis.title.x = element_text(hjust = 0.95), 
+          axis.title.y = element_text(vjust = 1, angle = 90), 
           axis.text.x = element_text(face = "bold", angle = 90), 
-          axis.text.y = element_text(face = "bold"), axis.line.x = element_line(color = "grey50", 
-                                                                                size = 0.5), panel.grid.major.x = element_blank(), 
+          axis.text.y = element_text(face = "bold"), 
+          axis.line.x = element_line(color = "grey50", size = 0.5), 
+          panel.grid.major.x = element_blank(), 
           panel.grid.major.y = element_line(size = 0.2, color = "grey90")) + 
-    labs(title = "Top-Authors' Production over Time", x = "Author", 
-         y = "Year") + geom_line(data = df2, aes(x = .data$Author, 
-                                                 y = .data$year, group = .data$Author), size = 1, color = "firebrick4", 
-                                 alpha = 0.3) + scale_x_discrete(limits = rev(levels(df2$Author))) + 
+    labs(title = "Top-Authors' Production over Time", x = "Author", y = "Year") + 
+    geom_line(data = df2, aes(x = .data$Author, y = .data$year, group = .data$Author), 
+              size = 1, color = "firebrick4", alpha = 0.3) + 
+    scale_x_discrete(limits = rev(levels(df2$Author))) + 
     coord_flip() #+ annotation_custom(logo, xmin = x[1], xmax = x[2], ymin = y[1], ymax = y[2])
   df$DOI = as.character(df$DOI)
   res <- list(dfAU = df2, dfPapersAU = df, graph = g)
