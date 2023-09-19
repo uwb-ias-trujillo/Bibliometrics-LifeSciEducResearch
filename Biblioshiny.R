aeb44092835fdda97935870304b05b6ecbd5910a
Caleb Trujillo <16155826+calebtru@users.noreply.github.com>
2023-09-19 10:50:28 -0700

    removing unused csv files and old r functions and RData files

--- Biblioshiny.R
+++
@@ -1,7 +0,0 @@
-#This script launches the biblioshiny script (previously named DCA.R)
-##### Install packages ###
-install.packages(c("tidyverse","devtools","rlang", "bibliometrix")) # to install the bibliometrix most recent version from GITHUB we use the devtools
-library('tidyverse') # load tidyverse package
-library('bibliometrix')
-
-biblioshiny()
